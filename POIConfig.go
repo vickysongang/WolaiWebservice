a45a0546181b63644de8de9c65f0f3ccf5d77154
LIU Tan <liutan@gmail.com>
2015-08-10 12:13:59 +0800

    added: TOML config

---
+++ POIConfig.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"time"
+)
+
+type POIConfig struct {
+	Title     string
+	Server    serverConf
+	Database  databaseConf
+	Redis     redisConf
+	LeanCloud leancloudConf
+	Reminder  reminderConf
+}
+
+type serverConf struct {
+	Port string
+}
+
+type databaseConf struct {
+	Username string
+	Password string
+	Method   string
+	Address  string
+	Port     string
+	Database string
+}
+
+type redisConf struct {
+	Host     string
+	Port     string
+	Db       int64
+	Password string
+}
+
+type leancloudConf struct {
+	AppId     string
+	AppKey    string
+	MasterKey string
+}
+
+type reminderConf struct {
+	Durations []duration
+}
+
+// Parse duration
+type duration struct {
+	time.Duration
+}
+
+func (d *duration) UnmarshalText(text []byte) error {
+	var err error
+	d.Duration, err = time.ParseDuration(string(text))
+	return err
+}
