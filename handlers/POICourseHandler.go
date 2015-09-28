9f4f3a17e9a9dba569ae82a98c0cc7f16ac8e486
vickysongang <vickysongang@gmail.com>
2015-09-28 12:12:22 +0800

    Course Handler

---
+++ handlers/POICourseHandler.go
@@ -0,0 +1,28 @@
+// POICourseHandler
+package handlers
+
+import (
+	"POIWolaiWebService/models"
+	"POIWolaiWebService/utils"
+	"time"
+
+	"github.com/astaxie/beego/toolbox"
+	seelog "github.com/cihub/seelog"
+)
+
+func POICourseExpiredHandler() {
+	expiredTask := toolbox.NewTask("expiredTask", "0 1 0 * * *", func() error {
+		processTime := time.Now().Format(utils.TIME_FORMAT)
+		seelog.Debug("Process expired course:", processTime)
+		userToCourses, _ := models.QueryExpiredCourses(processTime)
+		for _, userToCourse := range userToCourses {
+			updateInfo := map[string]interface{}{
+				"Status": "expired",
+			}
+			models.UpdateUserCourseInfoById(userToCourse.Id, updateInfo)
+		}
+		return nil
+	})
+	toolbox.AddTask("expiredTask", expiredTask)
+	toolbox.StartTask()
+}
