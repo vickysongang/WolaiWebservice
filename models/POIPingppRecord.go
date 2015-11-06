78d81293938d28d475af7d66b93192b4be245bc4
vickysongang <vickysongang@gmail.com>
2015-11-06 16:45:37 +0800

    Modified:pingpp

---
+++ models/POIPingppRecord.go
@@ -0,0 +1,64 @@
+// POIPingppRecord
+package models
+
+import (
+	"time"
+
+	"POIWolaiWebService/utils"
+
+	"github.com/astaxie/beego/orm"
+	"github.com/cihub/seelog"
+)
+
+type POIPingppRecord struct {
+	Id         int64     `json:"id" orm:"pk"`
+	Phone      string    `json:"phone"`
+	ChargeId   string    `json:"chargeId"`
+	OrderNo    string    `json:"orderNo"`
+	Amount     uint64    `json:"amount"`
+	Channel    string    `json:"channel"`
+	Currency   string    `json:"currency"`
+	Subject    string    `json:"subject"`
+	Body       string    `json:"body"`
+	Result     string    `json:"result"`
+	Comment    string    `json:"comment"`
+	RefundId   string    `json:"refundId"`
+	CreateTime time.Time `json:"-" orm:"auto_now_add;type(datetime)"`
+}
+
+func (record *POIPingppRecord) TableName() string {
+	return "pingpp_record"
+}
+
+func init() {
+	orm.RegisterModel(new(POIPingppRecord))
+}
+
+func InsertPingppRecord(record *POIPingppRecord) (*POIPingppRecord, error) {
+	o := orm.NewOrm()
+	id, err := o.Insert(record)
+	record.Id = id
+	return record, err
+}
+
+func UpdatePingppRecord(chargeId string, recordInfo map[string]interface{}) {
+	o := orm.NewOrm()
+	var params orm.Params = make(orm.Params)
+	for k, v := range recordInfo {
+		params[k] = v
+	}
+	_, err := o.QueryTable("pingpp_record").Filter("charge_id", chargeId).Update(params)
+	if err != nil {
+		seelog.Error("charge_id:", chargeId, " recordInfo:", recordInfo, " ", err.Error())
+	}
+}
+
+func QueryPingppRecordByChargeId(chargeId string) (*POIPingppRecord, error) {
+	o := orm.NewOrm()
+	qb, _ := orm.NewQueryBuilder(utils.DB_TYPE)
+	record := POIPingppRecord{}
+	qb.Select("id,phone,charge_id,order_no,amount,channel,currency,subject,body,result,comment,refund_id").From("pingpp_record").Where("charge_id = ?")
+	sql := qb.String()
+	err := o.Raw(sql, chargeId).QueryRow(&record)
+	return &record, err
+}
