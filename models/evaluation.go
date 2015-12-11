e66116a78924b21e8b5965cea9f5bedd97dc266c
LIU Tan <liutan@gmail.com>
2015-12-11 16:10:48 +0800

    modified: evaluation

---
+++ models/evaluation.go
@@ -0,0 +1,63 @@
+package models
+
+import (
+	"time"
+
+	"github.com/astaxie/beego/orm"
+
+	"WolaiWebservice/utils"
+)
+
+type Evaluation struct {
+	Id         int64     `json:"-" orm:"column(id);pk"`
+	UserId     int64     `json:"userId" orm:"column(user_id)"`
+	SessionId  int64     `json:"sessionId" orm:"column(session_id"`
+	Content    string    `json:"content" orm:"column(content)"`
+	CreateTime time.Time `json:"createTime" orm:"column(create_time);type(datetime);auto_now"`
+}
+
+func init() {
+	orm.RegisterModel(new(Evaluation))
+}
+
+func (e *Evaluation) TableName() string {
+	return "evaluation"
+}
+
+func CreateEvaluation(evalution *Evaluation) (*Evaluation, error) {
+	o := orm.NewOrm()
+	id, err := o.Insert(evalution)
+	if err != nil {
+		return nil, err
+	}
+	evalution.Id = id
+	return evalution, nil
+}
+
+func QueryEvaluation4Self(userId, sessionId int64) (*Evaluation, error) {
+	o := orm.NewOrm()
+	qb, _ := orm.NewQueryBuilder(utils.DB_TYPE)
+	qb.Select("id,user_id,session_id,content,create_time").From("evaluation").
+		Where("user_id = ? and session_id = ?")
+	sql := qb.String()
+	evalution := Evaluation{}
+	err := o.Raw(sql, userId, sessionId).QueryRow(&evalution)
+	if err != nil {
+		return nil, err
+	}
+	return &evalution, nil
+}
+
+func QueryEvaluation4Other(userId, sessionId int64) (*Evaluation, error) {
+	o := orm.NewOrm()
+	qb, _ := orm.NewQueryBuilder(utils.DB_TYPE)
+	qb.Select("id,user_id,session_id,content,create_time").From("evaluation").
+		Where("user_id <> ? and session_id = ?")
+	sql := qb.String()
+	evalution := Evaluation{}
+	err := o.Raw(sql, userId, sessionId).QueryRow(&evalution)
+	if err != nil {
+		return nil, err
+	}
+	return &evalution, nil
+}
