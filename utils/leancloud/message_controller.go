07369a569de2f3fc9131d6602c5e50518461c219
LIU Tan <liutan@gmail.com>
2016-01-04 16:39:45 +0800

    added course confirm message

--- utils/leancloud/message_controller.go
+++ utils/leancloud/message_controller.go
@@ -612,3 +612,41 @@ func SendOrderPersonalTutorExpireMsg(orderId int64) {
 
 	LCSendSystemMessage(USER_SYSTEM_MESSAGE, order.Creator, order.TeacherId, &lcTMsg)
 }
+
+func SendCourseChapterCompleteMsg(purchaseId, chapterId int64) {
+	var err error
+
+	purchase, err := models.ReadCoursePurchaseRecord(purchaseId)
+	if err != nil {
+		return
+	}
+
+	course, err := models.ReadCourse(purchase.CourseId)
+	if err != nil {
+		return
+	}
+
+	chapter, err := models.ReadCourseChapter(chapterId)
+	if err != nil {
+		return
+	}
+
+	if chapter.CourseId != course.Id {
+		return
+	}
+
+	text := fmt.Sprintf("%s\n第%d课时 %s\n导师标记该课时已完成",
+		course.Name,
+		chapter.Period, chapter.Title)
+
+	attr := make(map[string]string)
+	attr["accessRight"] = "[3]"
+
+	lcTMsg := LCTypedMessage{
+		Type:      LC_MSG_SYSTEM,
+		Text:      text,
+		Attribute: attr,
+	}
+
+	LCSendSystemMessage(USER_SYSTEM_MESSAGE, purchase.UserId, purchase.TeacherId, &lcTMsg)
+}
