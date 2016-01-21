69bec7bbee303c35ecbaa0725d8904e026ff102c
LIU Tan <liutan@gmail.com>
2016-01-21 16:57:13 +0800

    modified: dummy handlers

--- handlers/handlerv2/dummy.go
+++ handlers/handlerv2/dummy.go
@@ -30,27 +30,36 @@ func Dummy2(w http.ResponseWriter, r *http.Request) {
 	objectId := vars["objectId"][0]
 
 	leancloud.LCGetIntallation(objectId)
-	// userIdStr := r.Header.Get("X-Wolai-ID")
-	// userId, err := strconv.ParseInt(userIdStr, 10, 64)
-	// if err != nil {
-	// 	http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-	// 	return
-	// }
-
-	// manager := token.GetTokenManager()
-	// tokenString, err := manager.GenerateToken(userId)
-
-	// var status int64
-	// var errMsg string
-	// if err != nil {
-	// 	status = 2
-	// 	errMsg = err.Error()
-	// }
-
-	// json.NewEncoder(w).Encode(response.NewResponse(status, errMsg, tokenString))
 }
 
 func Dummy3(w http.ResponseWriter, r *http.Request) {
+	defer response.ThrowsPanicException(w, response.NullObject)
+	err := r.ParseForm()
+	if err != nil {
+		seelog.Error(err.Error())
+	}
+
+	userIdStr := r.Header.Get("X-Wolai-ID")
+	userId, err := strconv.ParseInt(userIdStr, 10, 64)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+
+	manager := token.GetTokenManager()
+	tokenString, err := manager.GenerateToken(userId)
+
+	var status int64
+	var errMsg string
+	if err != nil {
+		status = 2
+		errMsg = err.Error()
+	}
+
+	json.NewEncoder(w).Encode(response.NewResponse(status, errMsg, tokenString))
+}
+
+func Dummy4(w http.ResponseWriter, r *http.Request) {
 	defer response.ThrowsPanicException(w, response.NullObject)
 	err := r.ParseForm()
 	if err != nil {
