ca949dcbb1add11612774c7b06acb9119cff4083
vickysongang <vickysongang@gmail.com>
2016-04-20 16:36:09 +0800

    Added:course audition record in course list of student

--- controllers/course/course_list_student.go
+++ controllers/course/course_list_student.go
@@ -21,8 +21,19 @@ func GetCourseListStudent(userId, page, count int64) (int64, []*courseStudentLis
 
 	items := make([]*courseStudentListItem, 0)
 
+	var auditionUncompleteRecords []*models.CourseAuditionRecord
+
+	_, err := o.QueryTable("course_renew_record").Filter("user_id", userId).
+		Exclude("status", models.AUDITION_RECORD_STATUS_COMPLETE).
+		OrderBy("-last_update_time").All(&auditionUncompleteRecords)
+
+	for _, auditionRecord := range auditionUncompleteRecords {
+		item := assignAuditionCourseInfo(auditionRecord.CourseId, userId, auditionRecord.Status)
+		items = append(items, item)
+	}
+
 	var records []*models.CoursePurchaseRecord
-	_, err := o.QueryTable("course_purchase_record").Filter("user_id", userId).
+	_, err = o.QueryTable("course_purchase_record").Filter("user_id", userId).
 		OrderBy("-last_update_time").Offset(page * count).Limit(count).All(&records)
 	if err != nil {
 		return 0, items
@@ -50,5 +61,39 @@ func GetCourseListStudent(userId, page, count int64) (int64, []*courseStudentLis
 
 		items = append(items, &item)
 	}
+	if int64(len(records)) < count {
+		var auditionCompleteRecords []*models.CourseAuditionRecord
+		o.QueryTable("course_renew_record").Filter("user_id", userId).
+			Filter("status", models.AUDITION_RECORD_STATUS_COMPLETE).
+			OrderBy("-last_update_time").All(&auditionCompleteRecords)
+
+		for _, auditionRecord := range auditionCompleteRecords {
+			item := assignAuditionCourseInfo(auditionRecord.CourseId, userId, auditionRecord.Status)
+			items = append(items, item)
+		}
+	}
+
 	return 0, items
+}
+
+func assignAuditionCourseInfo(courseId, userId int64, status string) *courseStudentListItem {
+	course, err := models.ReadCourse(courseId)
+	if err != nil {
+		return nil
+	}
+
+	studentCount := courseService.GetCourseStudentCount(courseId)
+	chapterCount := int64(1)
+
+	chapterCompletePeriod, _ := courseService.QueryLatestCourseChapterPeriod(courseId, userId)
+
+	item := courseStudentListItem{
+		Course:                 *course,
+		StudentCount:           studentCount,
+		ChapterCount:           chapterCount,
+		AuditionStatus:         status,
+		PurchaseStatus:         status,
+		ChapterCompletedPeriod: chapterCompletePeriod,
+	}
+	return &item
 }
