45f5440ddfd5c0ff911b98da69843537b70451c9
vickysongang <vickysongang@gmail.com>
2016-04-23 14:40:11 +0800

    Upgrade course next chapter

---
+++ controllers/course/course_action_nextchapter_upgrade.go
@@ -0,0 +1,179 @@
+// course_action_nextchapter_upgrade
+package course
+
+import (
+	"errors"
+
+	"github.com/astaxie/beego/orm"
+
+	"WolaiWebservice/models"
+	courseService "WolaiWebservice/service/course"
+	"WolaiWebservice/utils/leancloud/lcmessage"
+)
+
+func HandleCourseActionNextChapterUpgrade(userId, studentId, courseId, chapterId int64) (int64, error) {
+	var err error
+	_, err = models.ReadUser(userId)
+	if err != nil {
+		return 2, errors.New("用户信息异常")
+	}
+
+	_, err = models.ReadUser(studentId)
+	if err != nil {
+		return 2, errors.New("用户信息异常")
+	}
+
+	var course *models.Course
+	if courseId == 0 { //代表试听课，从H5页面跳转过来的
+		course = courseService.QueryAuditionCourse()
+		if course == nil {
+			return 2, errors.New("课程信息异常")
+		}
+	} else {
+		course, err = models.ReadCourse(courseId)
+		if err != nil {
+			return 2, nil
+		}
+	}
+	if course.Type == models.COURSE_TYPE_DELUXE {
+		status, err := HandleDeluxeCourseNextChapterUpgrade(userId, studentId, courseId, chapterId)
+		return status, err
+	} else if course.Type == models.COURSE_TYPE_AUDITION {
+		status, err := HandleAuditionCourseNextChapterUpgrade(userId, studentId, courseId, chapterId)
+		return status, err
+	}
+	return 0, nil
+}
+
+func HandleDeluxeCourseNextChapterUpgrade(userId, studentId, courseId, chapterId int64) (int64, error) {
+	var err error
+	o := orm.NewOrm()
+
+	chapter, err := models.ReadCourseCustomChapter(chapterId)
+	if err != nil {
+		return 2, errors.New("课程信息异常")
+	}
+
+	var purchase models.CoursePurchaseRecord
+	err = o.QueryTable("course_purchase_record").
+		Filter("course_id", courseId).Filter("user_id", studentId).One(&purchase)
+	if err != nil {
+		return 2, errors.New("课程信息异常")
+	}
+	if purchase.TeacherId != userId {
+		return 2, errors.New("课程信息异常")
+	}
+
+	latestPeriod, err := courseService.QueryLatestCourseChapterPeriod(courseId, studentId)
+	if err == nil {
+		if chapter.Period != latestPeriod+1 {
+			return 2, errors.New("课程信息异常")
+		}
+
+		if purchase.PurchaseStatus != models.PURCHASE_RECORD_STATUS_PAID {
+			return 2, errors.New("学生尚未完成课程支付")
+		}
+
+	} else {
+		if latestPeriod != 0 {
+			return 2, errors.New("课程信息异常")
+		}
+
+		if purchase.AuditionStatus != models.PURCHASE_RECORD_STATUS_PAID {
+			return 2, errors.New("学生尚未完成试听支付")
+		}
+	}
+
+	record := models.CourseChapterToUser{
+		CourseId:  courseId,
+		ChapterId: chapterId,
+		UserId:    studentId,
+		TeacherId: userId,
+		Period:    chapter.Period,
+	}
+
+	_, err = models.CreateCourseChapterToUser(&record)
+	if err != nil {
+		return 2, errors.New("服务器操作异常")
+	}
+
+	go lcmessage.SendCourseChapterCompleteMsg(purchase.Id, chapter.Id)
+
+	chapterCount := purchase.ChapterCount
+
+	recordInfo := map[string]interface{}{
+		"audition_status": purchase.AuditionStatus,
+	}
+	models.UpdateCoursePurchaseRecord(purchase.Id, recordInfo)
+
+	if chapter.Period == 0 {
+		recordInfo := map[string]interface{}{
+			"audition_status": models.PURCHASE_RECORD_STATUS_COMPLETE,
+		}
+		models.UpdateCoursePurchaseRecord(purchase.Id, recordInfo)
+	} else if chapter.Period == chapterCount-1 {
+		recordInfo := map[string]interface{}{
+			"purchase_status": models.PURCHASE_RECORD_STATUS_COMPLETE,
+		}
+		models.UpdateCoursePurchaseRecord(purchase.Id, recordInfo)
+	}
+
+	return 0, nil
+}
+
+func HandleAuditionCourseNextChapterUpgrade(userId, studentId, courseId, chapterId int64) (int64, error) {
+	var err error
+	o := orm.NewOrm()
+
+	chapter, err := models.ReadCourseCustomChapter(chapterId)
+	if err != nil {
+		return 2, errors.New("课程信息异常")
+	}
+
+	var auditionRecord models.CourseAuditionRecord
+	err = o.QueryTable("course_audition_record").
+		Filter("course_id", courseId).Filter("user_id", studentId).One(&auditionRecord)
+	if err != nil {
+		return 2, errors.New("课程信息异常")
+	}
+	if auditionRecord.TeacherId != userId {
+		return 2, errors.New("课程信息异常")
+	}
+
+	latestPeriod, err := courseService.QueryLatestCourseChapterPeriod(courseId, studentId)
+	if err == nil {
+		if chapter.Period != latestPeriod+1 {
+			return 2, errors.New("课程信息异常")
+		}
+
+		if auditionRecord.Status != models.AUDITION_RECORD_STATUS_PAID {
+			return 2, errors.New("学生尚未完成课程支付")
+		}
+
+	} else {
+		if latestPeriod != 0 {
+			return 2, errors.New("课程信息异常")
+		}
+	}
+
+	record := models.CourseChapterToUser{
+		CourseId:  courseId,
+		ChapterId: chapterId,
+		UserId:    studentId,
+		TeacherId: userId,
+		Period:    chapter.Period,
+	}
+
+	_, err = models.CreateCourseChapterToUser(&record)
+	if err != nil {
+		return 2, errors.New("服务器操作异常")
+	}
+
+	go lcmessage.SendAuditionCourseChapterCompleteMsg(auditionRecord.Id, chapter.Id)
+
+	recordInfo := map[string]interface{}{
+		"Status": models.AUDITION_RECORD_STATUS_COMPLETE,
+	}
+	models.UpdateCourseAuditionRecord(auditionRecord.Id, recordInfo)
+	return 0, nil
+}
--- utils/leancloud/lcmessage/course.go
+++ utils/leancloud/lcmessage/course.go
@@ -44,3 +44,41 @@ func SendCourseChapterCompleteMsg(purchaseId, chapterId int64) {
 
 	leancloud.LCSendSystemMessage(USER_SYSTEM_MESSAGE, purchase.UserId, purchase.TeacherId, &lcTMsg)
 }
+
+func SendAuditionCourseChapterCompleteMsg(auditionRecordId, chapterId int64) {
+	var err error
+
+	auditionRecord, err := models.ReadCourseAuditionRecord(auditionRecordId)
+	if err != nil {
+		return
+	}
+
+	course, err := models.ReadCourse(auditionRecord.CourseId)
+	if err != nil {
+		return
+	}
+
+	chapter, err := models.ReadCourseChapter(chapterId)
+	if err != nil {
+		return
+	}
+
+	if chapter.CourseId != course.Id {
+		return
+	}
+
+	text := fmt.Sprintf("%s\n第%d课时 %s\n导师标记该课时已完成",
+		course.Name,
+		chapter.Period, chapter.Title)
+
+	attr := make(map[string]string)
+	attr["accessRight"] = "[3]"
+
+	lcTMsg := leancloud.LCTypedMessage{
+		Type:      LC_MSG_SYSTEM,
+		Text:      text,
+		Attribute: attr,
+	}
+
+	leancloud.LCSendSystemMessage(USER_SYSTEM_MESSAGE, auditionRecord.UserId, auditionRecord.TeacherId, &lcTMsg)
+}
