2b5d7a19bb3de2bb4d7a17e278490aa008f13c86
Maocheng Li <maocheng@wolai.me>
2016-06-24 20:06:49 +0800

    [FIX][Bug-3099] add unit in register invitation records

--- models/register_invitation.go
+++ models/register_invitation.go
@@ -11,13 +11,16 @@ type RegisterInvitation struct {
 	Phone       string    `json:"phone"`
 	Inviter     int64     `json:"inviter"`
 	Amount      int64     `json:"amount"`
+	Unit        string    `json:"unit"`
 	CreateTime  time.Time `json:"-"`
 	ProcessFlag string    `json:"processFlag"`
 }
 
 const (
-	REGISTER_INVITATION_FLAG_YES = "Y"
-	REGISTER_INVITATION_FLAG_NO  = "N"
+	REGISTER_INVITATION_FLAG_YES    = "Y"
+	REGISTER_INVITATION_FLAG_NO     = "N"
+	REGISTER_INVITATION_UNIT_MINUTE = "MINUTE"
+	REGISTER_INVITATION_UNIT_CENT   = "CENT"
 )
 
 func init() {
